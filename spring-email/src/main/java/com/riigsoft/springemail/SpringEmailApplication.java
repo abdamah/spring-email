4986e85a45cee136eb0b7a4b18035f150a4197ca
Abdallah Mah <abdamoh1@gmail.com>
2021-08-25 12:39:04 +0530

    Initial Commit

--- spring-email/src/main/java/com/riigsoft/springemail/SpringEmailApplication.java
+++ spring-email/src/main/java/com/riigsoft/springemail/SpringEmailApplication.java
@@ -3,11 +3,40 @@ package com.riigsoft.springemail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/***
+ * @author Abdallah
+ * Spring mail api
+ */
 @SpringBootApplication
 public class SpringEmailApplication {
+
+	// You can use this with out controller to test
+	// Implementing CommandLineRunners interface or @EventListener(ApplicationReadyEvent.class)
+	/*@Autowired
+	private IEmailSenderService service;*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEmailApplication.class, args);
 	}
 
+/*	@Override
+	public void run(String... args) throws Exception {
+
+		service.sendSimpleMessage("test@gmail.com",
+				"This is Email Body with Attachment...",
+				"Email With Attachment"
+		);
+	}*/
+
+
+	/*@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail() throws MessagingException {
+
+		service.sendSimpleMessage("test@gmail.com",
+				"This is Email Body with Attachment...",
+				"Email With Attachment"
+		);
+
+	}*/
 }
+
